f671cfefd18b3a100e0a257e07c3fa388ba08b96
Robert Lopez <robert.lopez@yale.edu>
2020-05-14 22:50:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,6 +20,7 @@ def long_planeteer_calls(planeteer_calls)
   # Return the boolean result of this check
   planeteer_calls.any? do |element|
     element.length > 4 
+  end 
 end
 
 def find_valid_calls(planeteer_calls)
